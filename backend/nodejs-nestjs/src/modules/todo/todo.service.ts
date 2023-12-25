845b97fe05836dff15b5988acb15df214208e01b
Hendri <hendri.juhanda@gmail.com>
2023-12-26 00:46:06 +0700

    feat(nestjs): wip todo service

---
+++ backend/nodejs-nestjs/src/modules/todo/todo.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Todo } from './todo.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class TodoService {
+  constructor(
+    @InjectRepository(Todo) private todoRepository: Repository<Todo>,
+  ) {}
+
+  index(): Promise<Todo[]> {
+    return this.todoRepository.find();
+  }
+}
