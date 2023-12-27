c6993cfb90856a737fd67a26ebac784ab624f625
Hendri Juhanda <hendri.juhanda@gmail.com>
2023-12-28 02:42:16 +0700

    faet: added lando

--- install.sh
+++ install.sh
@@ -20,13 +20,13 @@ Choose_Backend() {
     echo "[1] Laravel (PHP)"
     echo "[2] NestJS (NodeJS)"
 
-    read -p "Type: " var
+    read -p "(1/2)?: " var
 
     CHOOSEN_BACKEND=$var
 
     while [[ $CHOOSEN_BACKEND != @(1|2) ]]
     do
-        echo "\x1B[31mInvalid choice! Please try again.\x1B[0m"
+        echo -e "\n\x1B[31mInvalid choice! Please try again.\x1B[0m\n"
 
         Choose_Backend
     done
@@ -37,21 +37,27 @@ Choose_Frontend() {
     echo "[1] Nuxt (Vue)"
     echo "[2] Next (React)"
 
-    read -p "Type: " var
+    read -p "(1/2)?: " var
 
     CHOOSEN_FRONTEND=$var
 
     while [[ $CHOOSEN_FRONTEND != @(1|2) ]]
     do
-        echo "\x1B[31mInvalid choice! Please try again.\x1B[0m"
+        echo -e "\n\x1B[31mInvalid choice! Please try again.\x1B[0m\n"
         
         Choose_Frontend
     done
 }
 
 Prerequisite
+
 Choose_Backend
+
+echo ""
+
 Choose_Frontend
+
+echo ""
 
 BACKENDS[0]="laravel"
 BACKENDS[1]="nestjs"
@@ -66,6 +72,10 @@ case $BACKEND in
 
     "laravel")
         echo "Build laravel ..."
+
+        BACKEND_SITE="http://php-laravel.lndo.site"
+        API_URL="http://php-laravel.lndo.site/api"
+        PMA_SITE="http://pma.php-laravel.lndo.site"
 
         (
             cp backend/seed.json backend/php-laravel/seed.json
@@ -86,9 +96,15 @@ case $BACKEND in
 
     "nestjs")
         echo "Build nestjs ..."
+
+        BACKEND_SITE="http://nodejs-nestjs.lndo.site"
+        API_URL="http://nodejs-nestjs.lndo.site/api"
+        PMA_SITE="http://pma.nodejs-nestjs.lndo.site"
     ;;
 
 esac
+
+echo ""
 
 case $FRONTEND in
 
@@ -98,6 +114,30 @@ case $FRONTEND in
 
     "next")
         echo "Build next ..."
+
+        FRONTEND_SITE="http://react-next.lndo.site"
+
+        (
+            cd frontend/react-next
+
+            cp .env.example .env
+
+            sed -i "s|API_URL=|API_URL=$API_URL|" .env
+
+            lando start
+            lando npm install
+            lando npm run build
+        )
     ;;
 
 esac
+
+echo ""
+
+echo "The system should be ready to use. Access with urls below:"
+echo ""
+echo -e "Backend Site\t: ${BACKEND_SITE}"
+echo -e "Frontend Site\t: ${FRONTEND_SITE}"
+echo -e "PHP My Admin\t: ${PMA_SITE}"
+echo ""
+echo "Enjoy!"
